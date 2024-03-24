01849c6c757b8fadd8adffdb9423fb2ebb1211ee
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-24 20:02:21 +0100

    Task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -22,9 +22,22 @@ def display_states():
         dic_of_states = models.storage.all(State)
     elif type(models.storage) is DBStorage:
         dic_of_states = models.storage.all("State")
-    print(dic_of_states)
     dics = [obj for obj in dic_of_states.values()]
     return (render_template('7-states_list.html', dics=dics))
+
+
+@my_app.route("/cities_by_states", strict_slashes=False)
+def display_cities():
+    """Display Cities"""
+    if type(models.storage) is FileStorage:
+        dic_of_states = models.storage.all(State)
+        dic_of_cities = models.storage.all(City)
+    elif type(models.storage) is DBStorage:
+        dic_of_states = models.storage.all("State")
+        dic_of_cities = models.storage.all("City")
+    dics = [obj for obj in dic_of_states.values()]
+    dicc = [obj for obj in dic_of_cities.values()]
+    return (render_template('8-cities_by_states.html', dics=dics, dicc=dicc))
 
 
 if __name__ == '__main__':
